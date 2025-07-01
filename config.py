ff91badea355111e7832e277d8618af05c36e8bc
“DocDim” <mdieffi@gmail.com>
2025-07-01 10:19:48 -0400

    07/01/2025 commits

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'fmjfdyhf57a9ce1e2bc3hsfdghjfhfhghe054fe8a2e95a3011c02eec49aa3b4936ef7133b8ff7')
     # This is the URL for the *backend* dataAccess API
     DATA_ACCESS_API_URL = os.environ.get('DATA_ACCESS_API_URL', 'http://127.0.0.1:5000/api/v1')
-
+    
     # --- ADD/CONFIRM THESE SESSION CONFIGS ---
     SESSION_COOKIE_SAMESITE = 'Lax' # Recommended for modern browsers to prevent CSRF
     SESSION_COOKIE_SECURE = False # Set to True in production if using HTTPS
